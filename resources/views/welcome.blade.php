27c7eee863906f71974ce214ec3cadb0b17c6c1c
Lioosh12 <julchristian1339@gmail.com>
2025-01-15 07:12:47 +0700

    Perubahan Minor

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initialscale=1.0">
-    <title>Tubes Pemweb</title>
+    <title>Tubes PemWeb</title>
     @vite('resources/sass/app.scss')
 </head>
 
 <body>
     <div class="container text-center my-5">
         <h1 class="mb-4">Perpustakaan Kita Semua</h1>
-        {{-- Contoh cara mereferensikan gambar di dalam file blade dengan
-menggunakan pendekatan Vite --}}
         <img class="img-thumbnail" src="{{ Vite::asset('resources/images/books.png') }}" alt="image">
         <div class="col-md-2 offset-md-5 mt-4">
             <div class="d-grid gap-2">
